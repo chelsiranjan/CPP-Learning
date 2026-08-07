30a2cb93826ebf5a0100d8f8ac999d2daf29cae3
chelsiranjan <chelsiranjan04@gmail.com>
2026-08-07 13:21:08 +0530

    Initial commit

---
+++ CPP_Tutorial/02_print_numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << 3;
+  return 0;
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << 3 + 3;
+  return 0;
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << 3 * 5;
+  return 0;
+}
---
+++ CPP_Tutorial/03_newline.cpp
@@ -0,0 +1,48 @@
+// To insert a new line in your output, you can use the \n character:
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << "I am learning C++";
+  return 0;
+}
+
+
+
+
+// You can also use another << operator and place the \n character after the text, like this:
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << "\n";
+  cout << "I am learning C++";
+  return 0;
+}
+
+
+
+
+// Two \n characters after each other will create a blank line:
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << "\n\n";
+  cout << "I am learning C++";
+  return 0;
+}
+
+
+
+
+// Another way to insert a new line, is with the endl manipulator
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  cout << "I am learning C++";
+  return 0;
+}
